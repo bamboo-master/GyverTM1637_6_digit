7c9022377217a6f9632fbc83b7d2c56f4fba9a9c
bamboo-master <81521477+bamboo-master@users.noreply.github.com>
2022-12-28 12:03:15 +0300

    Update GyverTM1637_demo_mini.ino

--- examples/GyverTM1637_demo_mini/GyverTM1637_demo_mini.ino
+++ examples/GyverTM1637_demo_mini/GyverTM1637_demo_mini.ino
@@ -4,8 +4,8 @@
    AlexGyver Technologies http://alexgyver.ru/
 */
 
-#define CLK 2
-#define DIO 3
+#define CLK 12
+#define DIO 13
 
 #include "GyverTM1637.h"
 GyverTM1637 disp(CLK, DIO);
@@ -23,72 +23,13 @@ void setup() {
 void loop() {
   runningText();
   normClock();
-  scrollClock();
-  twistClock();
-  scrolls();
-  twists();  
   ints();
   fadeBlink();
 }
 
-void twists() {
-  disp.clear();
-  delay(200);
-  for (byte i = 0; i < 10; i++) {
-    disp.twist(3, i, 20);
-    delay(300);
-  }
-  
-  // скрутка массива БАЙТ
-  byte troll[4] = {_L, _O, _L, _empty};
-  disp.scrollByte(troll, 100);
-  delay(500);
-}
-
-void twistClock() {
-  byte hrs = 21, mins = 55;
-  uint32_t tmr;
-  Now = millis();
-  while (millis () - Now < 5000) {   // каждые 10 секунд
-    if (millis() - tmr > 500) {       // каждые полсекунды
-      tmr = millis();
-      flag = !flag;
-      disp.point(flag);   // выкл/выкл точки
-
-      if (flag) {
-        // ***** часы! ****
-        mins ++;
-        if (mins > 59) {
-          mins = 0;
-          hrs++;
-          if (hrs > 24) hrs = 0;
-        }
-        // ***** часы! ****
-        byte dispTime[] = {(byte)hrs / 10, (byte)hrs % 10, (byte)mins / 10, (byte)mins % 10};
-        disp.twist(dispTime, 35);    // выводим время массивом
-      }
-    }
-  }
-  disp.point(0);   // выкл точки
-}
-
-void scrolls() {
-  disp.clear();
-  delay(1000);
-  for (byte i = 0; i < 10; i++) {
-    disp.scroll(3, i, 50);
-    delay(300);
-  }
-
-  // прокрутка массива БАЙТ
-  byte troll[4] = {_t, _r, _o, _l};
-  disp.scrollByte(troll, 100);
-  delay(500);
-}
-
 void fadeBlink() {
   // пишем HELL
-  disp.displayByte(_H, _E, _L, _L);
+  disp.displayByte(_H, _E, _L, _L, _O, _U);
 
   Now = millis();
   while (millis () - Now < 3000) {    // 3 секунды
@@ -103,35 +44,10 @@ void fadeBlink() {
   }
 }
 
-void scrollClock() {
-  byte hrs = 15, mins = 0;
-  uint32_t tmr;
-  Now = millis();
-  while (millis () - Now < 5000) {   // каждые 10 секунд
-    if (millis() - tmr > 500) {       // каждые полсекунды
-      tmr = millis();
-      flag = !flag;
-      disp.point(flag);   // выкл/выкл точки
 
-      if (flag) {
-        // ***** часы! ****
-        mins ++;
-        if (mins > 59) {
-          mins = 0;
-          hrs++;
-          if (hrs > 24) hrs = 0;
-        }
-        // ***** часы! ****
-        byte dispTime[] = {(byte)hrs / 10, (byte)hrs % 10, (byte)mins / 10, (byte)mins % 10};
-        disp.scroll(dispTime, 70);    // выводим время массивом
-      }
-    }
-  }
-  disp.point(0);   // выкл точки
-}
 
 void normClock() {
-  byte hrs = 15, mins = 0;
+  byte hrs = 15, mins = 0, degs = 25;
   uint32_t tmr;
   Now = millis();
   while (millis () - Now < 5000) {   // 10 секунд
@@ -148,7 +64,7 @@ void normClock() {
         if (hrs > 24) hrs = 0;
       }
       // ***** часы! ****
-      disp.displayClock(hrs, mins);   // выводим время функцией часов
+      disp.displayClock(hrs, mins, degs);   // выводим время функцией часов
     }
   }
   disp.point(0);   // выкл точки
@@ -176,7 +92,7 @@ void ints() {
 
 void runningText() {
   byte welcome_banner[] = {_H, _E, _L, _L, _O, _empty, _empty,
-                           _T, _M, _1, _6, _3, _7,
+                           _t, _n, _1, _6, _3, _7,
                           };
   disp.runningString(welcome_banner, sizeof(welcome_banner), 150);  // 200 это время в миллисекундах!
 }
