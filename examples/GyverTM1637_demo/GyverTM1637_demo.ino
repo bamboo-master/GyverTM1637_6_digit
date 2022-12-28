1d7c736ce80f4d6527f6b8786cf33b26177af36c
bamboo-master <81521477+bamboo-master@users.noreply.github.com>
2022-12-28 12:02:17 +0300

    Update GyverTM1637_demo.ino

--- examples/GyverTM1637_demo/GyverTM1637_demo.ino
+++ examples/GyverTM1637_demo/GyverTM1637_demo.ino
@@ -4,8 +4,8 @@
    AlexGyver Technologies http://alexgyver.ru/
 */
 
-#define CLK 2
-#define DIO 3
+#define CLK 12
+#define DIO 13
 
 #include "GyverTM1637.h"
 GyverTM1637 disp(CLK, DIO);
@@ -22,98 +22,10 @@ void setup() {
 
 void loop() {
   runningText();
-  scrolls();
-  scrollClock();
-  twists();
-  twistClock();
   ints();
   bytes();
   fadeBlink();
   normClock();
-}
-
-void twists() {
-  // скручивание массив ЦИФР
-  byte digs[4] = {3, 5, 7, 1};
-  disp.twist(digs, 50);     // скорость прокрутки 100
-  delay(1000);
-
-  // скручивание прицельно (ячейка, БАЙТ, скорость)
-  disp.twistByte(0, _1, 50);
-  delay(1000);
-
-  // скручивание прицельно (ячейка, ЦИФРА, скорость)
-  disp.twist(0, 8, 70);
-  delay(1000);
-
-  disp.clear();
-  delay(200);
-  for (byte i = 0; i < 10; i++) {
-    disp.twist(3, i, 20);
-    delay(200);
-  }
-
-  // скручивание массива БАЙТ
-  byte troll[4] = {_t, _r, _o, _l};
-  disp.twistByte(troll, 50);
-  delay(1000);
-
-  // прицельное скручивание БАЙТА (ячейка, байт, скорость)
-  disp.twistByte(2, _G, 50);
-  delay(1000);
-}
-
-void twistClock() {
-  byte hrs = 21, mins = 55;
-  uint32_t tmr;
-  Now = millis();
-  while (millis () - Now < 10000) {   // каждые 10 секунд
-    if (millis() - tmr > 500) {       // каждые полсекунды
-      tmr = millis();
-      flag = !flag;
-      disp.point(flag);   // выкл/выкл точки
-
-      if (flag) {
-        // ***** часы! ****
-        mins ++;
-        if (mins > 59) {
-          mins = 0;
-          hrs++;
-          if (hrs > 24) hrs = 0;
-        }
-        // ***** часы! ****
-        disp.displayClockTwist(hrs, mins, 35);    // выводим время
-      }
-    }
-  }
-  disp.point(0);   // выкл точки
-}
-
-void scrolls() {
-  // прокрутка массив ЦИФР
-  byte digs[4] = {3, 5, 7, 1};
-  disp.scroll(digs, 100);     // скорость прокрутки 100
-  delay(1000);
-
-  // прокрутка прицельно (ячейка, ЦИФРА, скорость)
-  disp.scroll(0, 8, 200);
-  delay(1000);
-
-  disp.clear();
-  delay(1000);
-  for (byte i = 0; i < 10; i++) {
-    disp.scroll(3, i, 50);
-    delay(400);
-  }
-
-  // прокрутка массива БАЙТ
-  byte troll[4] = {_t, _r, _o, _l};
-  disp.scrollByte(troll, 100);
-  delay(1000);
-
-  // прицельная прокрутка БАЙТА (ячейка, байт, скорость)
-  disp.scrollByte(2, _G, 50);
-  delay(1000);
 }
 
 void bytes() {
@@ -123,7 +35,7 @@ void bytes() {
   delay(1000);
 
   // выводим байты напрямую (4 в скобках)
-  disp.displayByte(_L, _O, _L, _empty);
+  disp.displayByte(_L, _O, _L, _empty, _empty, _empty);
   delay(1000);
 
   // выводим байты "прицельно"
@@ -136,7 +48,7 @@ void bytes() {
   delay(1000);
 
   // выводим цифры напрямую (4 в скобках)
-  disp.display(1, 2, 3, 4);
+  disp.display(1, 2, 3, 4, 5 ,6);
   delay(1000);
 
   // выводим цифры "прицельно"
@@ -146,7 +58,7 @@ void bytes() {
 
 void fadeBlink() {
   // пишем HELL
-  disp.displayByte(_H, _E, _L, _L);
+  disp.displayByte(_H, _E, _L, _L, _O, _U);
 
   Now = millis();
   while (millis () - Now < 3000) {    // 3 секунды
@@ -161,34 +73,8 @@ void fadeBlink() {
   }
 }
 
-void scrollClock() {
-  byte hrs = 15, mins = 0;
-  uint32_t tmr;
-  Now = millis();
-  while (millis () - Now < 10000) {   // каждые 10 секунд
-    if (millis() - tmr > 500) {       // каждые полсекунды
-      tmr = millis();
-      flag = !flag;
-      disp.point(flag);   // выкл/выкл точки
-
-      if (flag) {
-        // ***** часы! ****
-        mins ++;
-        if (mins > 59) {
-          mins = 0;
-          hrs++;
-          if (hrs > 24) hrs = 0;
-        }
-        // ***** часы! ****
-        disp.displayClockScroll(hrs, mins, 70);    // выводим время
-      }
-    }
-  }
-  disp.point(0);   // выкл точки
-}
-
 void normClock() {
-  byte hrs = 15, mins = 0;
+  byte hrs = 15, mins = 0, degs = 25;
   uint32_t tmr;
   Now = millis();
   while (millis () - Now < 10000) {   // каждые 10 секунд
@@ -205,7 +91,7 @@ void normClock() {
         if (hrs > 24) hrs = 0;
       }
       // ***** часы! ****
-      disp.displayClock(hrs, mins);   // выводим время функцией часов
+      disp.displayClock(hrs, mins, degs);   // выводим время функцией часов
     }
   }
   disp.point(0);   // выкл точки
